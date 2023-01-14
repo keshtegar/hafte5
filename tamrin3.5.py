9d6d86bcb886bc8fbb0419258874f6361f3e658b
maryam keshtegar <97081304+keshtegar@users.noreply.github.com>
2023-01-14 12:05:22 +0330

    Update tamrin3.5.py

--- tamrin3.5.py
+++ tamrin3.5.py
@@ -4,10 +4,8 @@ class Date:
         self.m = m
         self.d = d
 
-
     def show(self):
-        print(self.y, "_", self.m, "_", self.d)
-
+        print(self.y, "-", self.m, "-", self.d)
 
     def sum(self, d2):
         result = Date(None, None, None)
@@ -22,85 +20,87 @@ class Date:
         if result.m >= 12:
             result.m -= 12
             result.y += 1
-
         return result
-
 
     def sub(self, d2):
         result = Date(None, None, None)
-        result.y = self.y - d2.y
-        result.m = self.m - d2.m
-        result.d = self.d - d2.d
-
+        result.y= d2.y- self.y
+        result.m= d2.m- self.m
+        result.d= d2.d- self.d
         if result.d < 0:
             result.d += 30
             result.m -= 1
-
         if result.m < 12:
             result.m += 12
             result.y -= 1
 
         return result
 
-
-
-    def GetMonthName (self):
+    def month_name (self):
         
         if self.m == 1: 
-
-            month="farvardin 🌸🌸🌸"
-
+            month="Farvardin🌼🌼"
         elif self.m == 2:
-            month = "ordibehesht 🌼🌷🌺"
+            month = "Ordibehesht🌺"
         elif self.m == 3:
-            month ="khordad 🍋🍓"
-
+            month ="Khordad🍓🍓"
         elif self.m == 4:
-            month = "🍦🍧"
-
+            month = "Tir🍇"
         elif self.m == 5:
-            month = "Mordad🥂🥤"
-
+            month = "Mordad🔅"
         elif self.m == 6:
-            month = "Shahrivar🍒🍇🍇"
-
+            month = "Shahrivar💖"
         elif self.m == 7:
-            month = "Mehr🌿"
-
+            month = "Mehr🏵🏵"
         elif self.m == 8:
-            month = "Aban🍁🍁"
-
+            month = "Aban🍂"
         elif self.m == 9:
-            month = "Azar🍂 "            
-
+            month = "Azar🍁🍁"            
         elif self.m == 10:
-            month = "Dey💗💓"
-
+            month = "Dey"
         elif self.m == 11:
             month = "Bahman🎂🎂"
-
         elif self.m == 12:
-            month = "Esfand🙌"           
-
+            month = "Esfand😍😍"           
         return month
 
-             
-calender1 = Date(1401, 2, 12)
-calender2 = Date(1354, 12, 10)
+    def Valid_Date (self):
+        
+        if self.d < 1 or self.d > 31: 
 
-print("calender is: ")
+            day=("False Day")
+            return day
+        if self.m < 1 or self.m > 12: 
+
+            month=("False month")
+            return month
+        if self.y < 1 : 
+
+            year=("False Year")
+            return year
+        
+calender1 = Date(1360,11,25)
+calender2 = Date(1400,10,20)
+
+print("first date : ")
 calender1.show()
 
-print("calender2 is: ")
+print("second date :")
 calender2.show()
 
 s = calender1.sum(calender2)
-print("calender1 + calender2 = : ")
+print("Sum of 2 dates : ")
 s.show()
 
-m = calender1.sub(calender2)
-print("calender1 - calender2 = : ")
-m.show()
+Subtraction = calender1.sub(calender2)
+print("Subtraction of 2 dates  : ")
+Subtraction.show()
 
-MonthName = calender1.GetMonthName()
-print("month of calender is:\n", MonthName)
+Month_Name = calender2.month_name()
+print("Month name ", Month_Name)
+
+Valid_date = calender1.Valid_Date()
+print("False calender1:",   Valid_date)
+
+Valid_date = calender2.Valid_Date()
+print("False calender2:",   Valid_date)
